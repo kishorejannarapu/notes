74b16ade44172b701877e134e507d0313b9dd1a0
Kishore Kumar <kishore.jannarapu@gmail.com>
2023-12-22 16:57:12 +0800

    Create git-aliases.sh

---
+++ git-aliases.sh
@@ -0,0 +1,13 @@
+
+
+git config --global alias.ji 'checkout feature/jenkins-integration'
+
+git config --global alias.poh 'push origin HEAD'
+
+git config --global alias.pohf 'push -f origin HEAD'
+
+git config --global alias.pod 'pull origin develop'
+
+git config --global alias.cd 'checkout develop'
+
+git config --global alias.cm 'checkout master'
